54699054befad8c729b7d81d44754c4108d6fdda
nemo <nemo724@naver.com>
2025-01-17 23:50:13 +0900

    메인함수에 pyramid.h의 함수들 추가 및 pyramid.h 내 잘못 선언된 함수명 수정

--- 009번/a009_pyramid.c
+++ 009번/a009_pyramid.c
@@ -1,1 +1,23 @@
-﻿
+﻿#include "pyramid.h"
+
+int main(void)
+{first_pyramid();
+printf("\n");
+
+second_pyramid();
+printf("\n");
+
+third_pyramid();
+printf("\n");
+	
+fourth_pyramid();
+printf("\n");
+
+fifth_pyramid();
+printf("\n");
+
+sixth_pyramid();
+printf("\n");
+
+
+}
--- 009번/pyramid.h
+++ 009번/pyramid.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 
 void first_pyramid(void);
-void secont_pyramid(void);
+void second_pyramid(void);
 void third_pyramid(void);
 void fourth_pyramid(void);
 void fifth_pyramid(void);
-void sixth_pyrmaid(void);
+void sixth_pyramid(void);
 
 
 
