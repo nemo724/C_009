6b234bd315865f76ebe6be6afb29fd27eb9e18b8
nemo <nemo724@naver.com>
2025-01-17 22:32:07 +0900

    Revert "함수 second_pyramid 정의"
    This reverts commit c3e0f3fd8c2bf43de5d65c4ce17a2f9dea69d88d.

--- 009번/pyramid.c
+++ 009번/pyramid.c
@@ -7,11 +7,3 @@ void first_pyramid(void)
     printf("\n");
     }
 }
-
-void second_pyramid(void)
-{for(int layer=0; layer<5;layer++)
-    {for(int star=0;star<5-layer;star++)
-         {printf("*"); }
-    printf("\n");
-    }
-}
