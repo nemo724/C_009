4c62029cfb8b10ba2f0d95b6359cf5a0e25965d5
nemo <nemo724@naver.com>
2025-01-17 22:34:40 +0900

    second_pyramid 수정

--- 009번/pyramid.c
+++ 009번/pyramid.c
@@ -7,3 +7,13 @@ void first_pyramid(void)
     printf("\n");
     }
 }
+
+void second_pyramid(void)
+{int star_count=1;
+ for(int layer=0; layer<5;layer++)
+    {for(int star=0;star<star_count;star++)
+        {printf("*"); }
+    printf("\n");
+    star_count += 2;
+    }
+}
