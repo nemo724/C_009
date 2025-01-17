a7e40379a928afc096667ea7c0d12f7fcc0bddac
nemo <nemo724@naver.com>
2025-01-17 23:53:48 +0900

    fifth_pyramid 내 blank 출력 반복문 제어조건 수정

--- 009번/pyramid.c
+++ 009번/pyramid.c
@@ -41,13 +41,13 @@ void fourth_pyramid(void)
 void fifth_pyramid(void)
 {int star_count = 1;
 for(int layer =0; layer<5;layer++)
-    {for(int blank=0;blank<(8-star_count)/2;blank++)
+    {for(int blank=0;blank<(10-star_count)/2;blank++)
         {printf(" "); }
     
      for(int star=0;star<star_count;star++)
         {printf("*"); }
 
-     for(int blank=0;blank<(8-star_count)/2;blank++)
+     for(int blank=0;blank<(10-star_count)/2;blank++)
         {printf(" "); }
 
      printf("\n");
